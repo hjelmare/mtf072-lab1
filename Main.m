19086a3572d31d88fb27e022365fa2a108b358e5
Eiliv Hägg <eiliv@o7155b-04.o7155b.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2015-11-10 15:32:34 +0100

    small change

--- Main.m
+++ Main.m
@@ -11,6 +11,11 @@ T1 = 10;
 T2 = 20;
 c1 = 20;
 c2 = 0.2;
+edgesX = [0 .15 .2 .3 .4 .5 .6 .7 .8 .999 1.0];
+edgesY = [0 .2 .4 .6 .8 1.0];
+
+%Initializing mesh and temperature
+
 
 %Gauss-Seidel loop
 for i = 1:maxIterations
--- initializeMesh.m
+++ initializeMesh.m
@@ -1,10 +1,4 @@
-%function [T, pY, pX] = initializeMesh(edgesY, edgesX)
-
-clc
-clf
-clear all
-edgesX = [0 .15 .2 .3 .4 .5 .6 .7 .8 .999 1.0];
-edgesY = [0 .2 .4 .6 .8 1.0];
+function [T, pY, pX] = initializeMesh(edgesY, edgesX,T1,T2)
 
 edgesX = [2*edgesX(1)-edgesX(2) edgesX 2*edgesX(end)-edgesX(end-1)];
 edgesY = [2*edgesY(1)-edgesY(2) edgesY 2*edgesY(end)-edgesY(end-1)];
@@ -14,8 +8,8 @@ ptPosY = edgesY(1:end-1) + 0.5*diff(edgesY);
 
 
 outMesh = zeros(length(ptPosX),length(ptPosY));
-outMesh(1,:) = 10;
-outMesh(:,end) = 20;
+outMesh(1,:) = T1;
+outMesh(:,end) = T2;
 % b3 is insulating, so it gets dealt with elsewhere
 outMesh(:,1) = (1:length(ptPosX)) * (20/length(ptPosX));
 
