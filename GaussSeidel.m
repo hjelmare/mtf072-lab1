eb46bcdebe28f463393344e3c88ea1977a3d9b79
Eiliv Hägg <eiliv@o7155b-04.o7155b.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2015-11-10 16:16:12 +0100

    debugged

--- GaussSeidel.m
+++ GaussSeidel.m
@@ -4,32 +4,30 @@ function T = GaussSeidel(T,x,y,deltaX,deltaY,T1,c1,c2,kFactor)
     
     %Predefining coefficients that handle boundary conditions
     eCoeff = ones(rows,cols);
-    eCoeff(:,cols-1) = 0.5;
+    eCoeff(:,cols-1) = 2;
     wCoeff = ones(rows,cols);
-    wCoeff(:,2) = 0.5;
+    wCoeff(:,2) = 2;
     nCoeff = ones(rows,cols);
     nCoeff(rows-1,:) = 0;
     sCoeff = ones(rows,cols);
-    sCoeff(2,:) = 0.5;
+    sCoeff(2,:) = 2;
     
-    %Performing Gauss-Seidel update    
-    disp('hej')
+    %Performing Gauss-Seidel update   
     for j = 2:cols-1
         for i = 2:rows-1
-            disp([i j])
-            dXeast = (x(j+1) - x(j)) * eCoeff(i,j);
-            dXwest = (x(j) - x(j-1)) * wCoeff(i,j);
-            dYnorth = (y(i+1) - y(i)) * nCoeff(i,j);
-            dYsouth = (y(i) - y(i-1)) * sCoeff(i,j);
+            dXeast = (x(j+1) - x(j));
+            dXwest = (x(j) - x(j-1));
+            dYnorth = (y(i+1) - y(i));
+            dYsouth = (y(i) - y(i-1));
             Te = T(i,j+1);
             Tw = T(i,j-1);
             Tn = T(i+1,j);
             Ts = T(i-1,j);
             Tp = T(i,j);
-            ae = (deltaY(i)/dXeast) * 2 *(1 + 20*Te/T1)*kFactor;
-            aw = (deltaY(i)/dXwest) * 2 *(1 + 20*Tw/T1)*kFactor;
-            an = (deltaX(j)/dYnorth) * 2 *(1 + 20*Tn/T1)*kFactor;
-            as = (deltaX(j)/dYsouth) * 2 *(1 + 20*Ts/T1)*kFactor;
+            ae = (deltaY(i)/dXeast) * 2 *(1 + 20*Te/T1)*kFactor * eCoeff(i,j);
+            aw = (deltaY(i)/dXwest) * 2 *(1 + 20*Tw/T1)*kFactor * wCoeff(i,j);
+            an = (deltaX(j)/dYnorth) * 2 *(1 + 20*Tn/T1)*kFactor * nCoeff(i,j);
+            as = (deltaX(j)/dYsouth) * 2 *(1 + 20*Ts/T1)*kFactor * sCoeff(i,j);
             bp = -15*c2*Tp*deltaX(j)*deltaY(i);
             bu = 15*c1*deltaX(j)*deltaY(i);
             ap = ae + aw + an + as - bp;
