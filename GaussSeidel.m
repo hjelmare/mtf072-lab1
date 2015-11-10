d3537c78b7b5a9df79e81743a80ba74789708554
Eiliv Hägg <eiliv@o7155b-04.o7155b.studat.rh66.ii.2.hb.s.cdal.chalmers.se>
2015-11-10 15:23:42 +0100

    gauss-seidel

---
+++ GaussSeidel.m
@@ -0,0 +1,38 @@
+function T = GaussSeidel(T,x,y,deltaX,deltaY,T1,c1,c2,kFactor)
+
+    [rows,cols] = size(T);  
+    
+    %Predefining coefficients that handle boundary conditions
+    eCoeff = ones(rows,cols);
+    eCoeff(:,cols-1) = 0.5;
+    wCoeff = ones(rows,cols);
+    wCoeff(:,2) = 0.5;
+    nCoeff = ones(rows,cols);
+    nCoeff(rows-1,:) = 0;
+    sCoeff = ones(rows,cols);
+    sCoeff(2,:) = 0.5;
+    
+    %Performing Gauss-Seidel update    
+    for j = 2:cols-1
+        for i = 2:rows-1
+            dXeast = (x(j+1) - x(j)) * eCoeff(i,j);
+            dXwest = (x(j) - x(j-1)) * wCoeff(i,j);
+            dYnorth = (y(i+1) - y(i)) * nCoeff(i,j);
+            dYsouth = (y(i) - y(i-1)) * sCoeff(i,j);
+            Te = T(i,j+1);
+            Tw = T(i,j-1);
+            Tn = T(i+1,j);
+            Ts = T(i-1,j);
+            Tp = T(i,j);
+            ae = (deltaY(i)/dXeast) * 2 *(1 + 20*Te/T1)*kFactor;
+            aw = (deltaY(i)/dXwest) * 2 *(1 + 20*Tw/T1)*kFactor;
+            an = (deltaX(j)/dYnorth) * 2 *(1 + 20*Tn/T1)*kFactor;
+            as = (deltaX(j)/dYsouth) * 2 *(1 + 20*Ts/T1)*kFactor;
+            bp = -15*c2*Tp*deltaX(j)*deltaY(i);
+            bu = 15*c1*deltaX(j)*deltaY(i);
+            ap = ae + aw + an + as - bp;
+            T(i,j) = (ae*Te + aw*Tw + an*Tn + as*Ts + bu)/ap;   
+        end
+    end
+
+end
--- Main.m
+++ Main.m
@@ -1,12 +1,9 @@
-clc
-clf
-clear all
 %Main program in CFD assignment K1
 
 %Declaration of scalar variables
-maxIterations = 1000;
+maxIterations = 2000;
 meshSize = 10;
-initialT = 10;
+initialT = 5;
 kFactor = 1;
 L = 1;
 H = 1;
@@ -15,22 +12,13 @@ T2 = 20;
 c1 = 20;
 c2 = 0.2;
 
-% Give initializeMesh y- and x-locations of edges between cells
-distX = [0 1 2 3 4];
-distY = [0 1 2 3 4 5 6];
-
-%Initializing mesh
-[x,y] = initializeMesh(meshSize);
-
-%Initializing temperature
-T = ones(gridSize) * initialT;
-
 %Gauss-Seidel loop
 for i = 1:maxIterations
    
-    T = GaussSeidel(T,T1,T2,c1,c2,L,H,kFactor);
-    
-    
+    T = GaussSeidel(T,x,y,deltaX,deltaY,T1,c1,c2,kFactor);  
     
 end
 
+%Plotting result
+figure();
+contourf(y,x,T);
